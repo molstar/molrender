262be872a9f244d4e17b6424fcb3475ed88043a1
MarkMa2003 <ke.mark.ma2021@gmail.com>
2024-01-05 14:36:41 -0800

    add save state

--- src/render.ts
+++ src/render.ts
@@ -155,7 +155,7 @@ export class ImageRenderer {
     imagePass: ImagePass;
     assetManager = new AssetManager();
 
-    constructor(private width: number, private height: number, private format: 'png' | 'jpeg', private plddt: 'on' | 'single-chain' | 'off', focusFactory?: FocusFactoryI) {
+    constructor(private width: number, private height: number, private format: 'png' | 'jpeg', private plddt: 'on' | 'single-chain' | 'off', private save_state_flag: boolean = false, focusFactory?: FocusFactoryI) {
         this.webgl = createContext(getGLContext(this.width, this.height, {
             antialias: true,
             preserveDrawingBuffer: true,
@@ -217,6 +217,10 @@ export class ImageRenderer {
                         name: 'off',
                         params: {}
                     }
+                },
+                sharpening: {
+                    name: 'off', // or some other valid value
+                    params: {}
                 }
             }
         });
@@ -226,7 +230,8 @@ export class ImageRenderer {
             },
             multiSample: {
                 mode: 'on',
-                sampleLevel: 4
+                sampleLevel: 4,
+                reduceFlicker: true
             }
         });
         this.imagePass.setSize(this.width, this.height);
@@ -395,6 +400,16 @@ export class ImageRenderer {
         const structure = await StructureSymmetry.buildAssembly(modelStructure, symmetry.assemblies[asmIndex].id).run();
         const colorTheme = this.checkPlddtColorTheme(structure);
         await this.render(structure, `${outPath}/${fileName}_assembly-${asmId}`, { colorTheme });
+
+        this.saveState({
+            "id":fileName,
+            "case": "assembly",
+            "config": {
+                "props": {
+                    "assemblyId": asmId
+                }
+            }
+        },`${outPath}/${fileName}_assembly-${asmId}.json`)       
     }
 
     /**
@@ -406,6 +421,15 @@ export class ImageRenderer {
         const structure = Structure.ofModel(model);
         const colorTheme = this.checkPlddtColorTheme(structure);
         await this.render(structure, `${outPath}/${fileName}_model-${oneIndex}`, { colorTheme });
+        this.saveState({
+            "id":fileName,
+            "case": "model",
+            "config": {
+                "props": {
+                    "modelIndex": oneIndex
+                }
+            }
+        },`${outPath}/${fileName}_model-${oneIndex}.json`)
     }
 
     /**
@@ -420,6 +444,18 @@ export class ImageRenderer {
         }));
         const colorTheme = this.checkPlddtColorTheme(structure);
         await this.render(structure, `${outPath}/${fileName}_chain-${chainName}`, { colorTheme, suppressBranched: true });
+        this.saveState({
+            "id":fileName,
+            "case": "chain",
+            "config": {
+                "props": {
+                    "target" : {
+                        "kind":"chain",
+                        "asymId": chainName
+                    }
+                }
+            }
+        },`${outPath}/${fileName}_chain-${chainName}.json`)
     }
 
     async renderModels(models: Trajectory, outPath: string, fileName: string) {
@@ -534,7 +570,7 @@ export class ImageRenderer {
         if (PLDDTConfidenceColorThemeProvider.isApplicable({ structure })) return PLDDTConfidenceColorThemeProvider.name;
     }
 
-    private async render(structure: Structure, imagePathName: string, options?: { colorTheme?: string, suppressSurface?: boolean, suppressBranched?: boolean, structureSize?: StructureSize, quality?: VisualQuality }) {
+    private async render(structure: Structure, imagePathName: string, options?: { colorTheme?: string, suppressSurface?: boolean, suppressBranched?: boolean, structureSize?: StructureSize, quality?: VisualQuality}, molrenderState?:object ) {
         const size = options?.structureSize ?? getStructureSize(structure);
         const quality = options?.quality ?? getQuality(structure);
         let focusStructure: Structure;
@@ -601,4 +637,21 @@ export class ImageRenderer {
             await this.renderModels(trajectory, outPath, fileName);
         }
     }
+
+    private saveState(molrenderState: any, path:string): void {
+        if(!this.save_state_flag)
+            return;
+        const cameraState = this.canvas3d.camera.state
+        const savedState = {...molrenderState,cameraState:cameraState}
+        const stateJson = JSON.stringify(savedState, null, 2); // The '2' adds indentation for readability
+
+        fs.writeFile(path, stateJson, 'utf8', (err) => {
+            if (err) {
+                console.error('An error occurred while saving the state:', err);
+                return;
+            }
+            console.log('State saved successfully to', path);
+        });
+    }
+
 }
